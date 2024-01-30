22a6a43fc8018f5f070fd44936344b83c544f0ab
Souravtkrishna <149757958+Souravtkrishna@users.noreply.github.com>
2024-01-30 19:52:01 +0530

    Create alpha.py

---
+++ alpha.py
@@ -0,0 +1,9 @@
+n=int(input("Enter the number of names:"))
+names=[]
+for i in range(n):
+   name=input("enter names:")
+   names.append(name)
+names.sort()
+for name in names:
+   print(name)   
+   
